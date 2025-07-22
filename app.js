a800008b34b940cfb99762df0bbbdcf56b8a674d
Vimalraj-7202 <vimalrajsekar7202@gmail.com>
2025-07-22 17:56:12 +0530

    Cross origin issue fixed

--- app.js
+++ app.js
@@ -3,14 +3,9 @@ import cors from 'cors';
 
 const app = express();
 
-// ✅ Safer CORS for production:
-const allowedOrigins ='http://localhost:3000'
 
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
