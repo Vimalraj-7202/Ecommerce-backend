f2d25b7445907727b1d20af3a0ff7e9e08159223
Vimalraj-7202 <vimalrajsekar7202@gmail.com>
2025-07-22 17:40:20 +0530

    Cross origin issue fixed

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected successfully!');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB connected successfully!");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
