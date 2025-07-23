05b9c6bf3ad5f21fad6eb8d4ef25b521f141670e
Vimalraj-7202 <vimalrajsekar7202@gmail.com>
2025-07-23 10:00:19 +0530

    Env changed

--- config/db.js
+++ config/db.js
@@ -2,11 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(" MongoDB connected successfully!");
+    await mongoose.connect(process.env.MONGO_URL); 
+    console.log(" MongoDB connected successfully!")
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
     process.exit(1);
