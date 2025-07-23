2a3a02f274feda00f578ad3341eb939ca60ce2b2
Vimalraj-7202 <vimalrajsekar7202@gmail.com>
2025-07-23 09:45:53 +0530

    Env changed

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.get('/',(req,res)=>{
+    res.send('Ecommerce backed server is runing successfully!')
+})
 
 export default app;
